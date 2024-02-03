0b1554350e9263f02bab0e1bd6182cc00b6851a0
Delibasa Dejan <dejan.delibasa@gmail.com>
2024-02-03 11:19:29 +0200

    Added tags for detectable blocks

--- src/main/java/com/nodle66/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/com/nodle66/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,15 +1,22 @@
 package com.nodle66.tutorialmod.item.custom;
 
 import com.nodle66.tutorialmod.block.ModBlocks;
+import com.nodle66.tutorialmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings) {
@@ -47,6 +54,12 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE) || state.isOf(ModBlocks.RAW_RUBY_BLOCK);
+        return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.tutorialmod.metal_detector.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
