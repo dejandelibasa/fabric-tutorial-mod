19281e8859593b137ec5bb180acfe52b190d20d1
Delibasa Dejan <dejan.delibasa@gmail.com>
2024-02-07 21:44:48 +0200

    Uhm this is some stuff fr

---
+++ src/main/java/com/nodle66/tutorialmod/mixin/MinecartEntityMixin.java
@@ -0,0 +1,8 @@
+package com.nodle66.tutorialmod.mixin;
+
+import net.minecraft.entity.vehicle.MinecartEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin({MinecartEntity.class})
+public class MinecartEntityMixin {
+}
